0da84aad6a680719a399e2738db9da2f5a4daf44
ArifArfi <arifarfi35@gmail.com>
2020-11-25 20:23:15 +0700

    ArifArfi

---
+++ Destroyable.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public interface Destroyable {
+    public abstract void destroyed();
+}
---
+++ Plant.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Plant {
+    public void doDestroy(Destroyable d) {
+        d.destroyed();
+    }
+}
